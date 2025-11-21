76ff4f21f599de54be4040e96edf89905ff6afa9
nisalG <nimgunathilaka021@gmail.com>
2025-11-21 14:23:30 +0530

    feat: add ThemeProvider component and integrate next-themes for dark mode support; enhance Hero component layout

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,5 +1,6 @@
 import react from 'react';
 import { Spotlight } from './ui/Spotlight';
+import { cn } from '@/utils/cn';
 
 const Hero = () => {
     return (
@@ -8,7 +9,35 @@ const Hero = () => {
             <Spotlight className='-top-40 -left-10 md:-left-32 md:-top-20 h-screen' fill="white" />
             <Spotlight className='top-10 left-full h-[80vh] w-[50vw]' fill="purple" />
             <Spotlight className='top-28 left-80 h-[80vh] w-[50vw]' fill="blue" />
-          </div>  
+          </div> 
+
+          <div className="h-screen w-full items-center justify-center bg-white dark:bg-black -100 dark:bg-grid-white/[0.03] bg-grid-black/[0.02] absolute top-0 left-0">
+          <div
+            className={cn(
+              "absolute inset-0",
+              "[background-size:40px_40px]",
+              "[background-image:linear-gradient(to_right,#e4e4e7_1px,transparent_1px),linear-gradient(to_bottom,#e4e4e7_1px,transparent_1px)]",
+              "dark:[background-image:linear-gradient(to_right,#262626_1px,transparent_1px),linear-gradient(to_bottom,#262626_1px,transparent_1px)]",
+            )}
+          />
+
+          
+          {/* Radial gradient for the container to give a faded look */}
+          <div className="pointer-events-none absolute inset-0 flex items-center justify-center bg-white [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)] dark:bg-black"></div>
+         </div> 
+
+          <div className='flex justify-center relative my-20 z-10'>
+            <div className='max-w-[89vw] md:max-w-2xl lg:max-w-[60vm] flex flex-col items-center justify-center'>
+              <h2 className='uppercase tracking-widest text-xs text-center text-blue-100 max-w-80'>
+                Dynamic web Magic with Next.js
+              </h2>
+
+            </div>
+          </div>
+         
+
+
+
         </div>
     )
 }
