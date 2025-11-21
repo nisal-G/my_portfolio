63b6e399c6d13c4fe287673b3c50db05d68f6fd6
nisalG <nimgunathilaka021@gmail.com>
2025-11-21 10:39:24 +0530

    feat: enhance styling and animations with Tailwind CSS, add custom properties, and update Hero component

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,20 +1,53 @@
 import type { Config } from "tailwindcss";
 
-const config: Config = {
+const config = {
+  darkMode: "class",
   content: [
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./pages/**/*.{ts,tsx}",
+    "./components/**/*.{ts,tsx}",
+    "./app/**/*.{ts,tsx}",
+    "./src/**/*.{ts,tsx}",
+    "./data/**/*.{ts,tsx}",
   ],
   theme: {
     extend: {
       colors: {
-        DEFAULT: '#000',
-        100: '#00319'
-        }
+        black: {
+          DEFAULT: "#000",
+          100: "#000319",
+        },
+        white: {
+          DEFAULT: "#FFF",
+          100: "#BEC1DD",
+        },
+        blue: {
+          100: "#E4ECFF",
+        },
+        purple: "#CBACF9",
+      },
+      keyframes: {
+        spotlight: {
+          "0%": {
+            opacity: "0",
+            transform: "translate(-72%, -62%) scale(0.5)",
+          },
+          "100%": {
+            opacity: "1",
+            transform: "translate(-50%, -40%) scale(1)",
+          },
+        },
+        shimmer: {
+          "0%": { backgroundPosition: "0 0" },
+          "100%": { backgroundPosition: "-200% 0" },
+        },
+      },
+      animation: {
+        spotlight: "spotlight 2s ease 0.75s 1 forwards",
+        shimmer: "shimmer 2s linear infinite",
+      },
     },
   },
   plugins: [],
-};
+} satisfies Config;
 
 export default config;
