3e494e490d3ad22358b77e526dd9ba07fb24ef11
nisalG <nimgunathilaka021@gmail.com>
2025-11-22 11:13:22 +0530

    add Grid component and FloatingNav for enhanced layout and navigation

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,28 @@
+import Grid from "@/components/Grid";
 import Hero from "@/components/Hero";
+import { FloatingNav } from "@/components/ui/FloatingNav";
 import Image from "next/image";
+import { FaHome } from "react-icons/fa";
+import { FaH } from "react-icons/fa6";
 
 export default function Home() {
   return (
     <main className="relative bg-black -100 flex justify-center items-center flex-col overflow-hidden mx-auto sm:px-10 px-5">
       <div className="max-w-7xl w-full">
+
+        <FloatingNav
+          navItems={[
+            { name: "Home", link: "/" , icon: <FaHome />},
+            { name: "About", link: "/about" },
+            { name: "Projects", link: "/projects" },
+            { name: "Contact", link: "/contact" },
+          ]}
+        />
+
         <Hero />
+
+        <Grid />
+
       </div>
     </main>
   );
