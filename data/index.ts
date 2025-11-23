8942d83fdb282198070b57aa8b4a7d377721d33c
nisalG <nimgunathilaka021@gmail.com>
2025-11-23 22:08:30 +0530

    Update grid item title and clear description for improved clarity

--- data/index.ts
+++ data/index.ts
@@ -18,8 +18,8 @@ export const gridItems = [
   },
   {
     id: 2,
-    title: "Turning complex problems into simple solutions",
-    description: "Breaking down challenges into manageable, elegant code",
+    title: "I'm very flexible with time zone communications",
+    description: "",
     className: "lg:col-span-2 md:col-span-3 md:row-span-2",
     imgClassName: "",
     titleClassName: "justify-start",
