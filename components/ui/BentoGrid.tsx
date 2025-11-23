a4bf455bebbc9bd96452615e5c165b26659552cd
nisalG <nimgunathilaka021@gmail.com>
2025-11-23 19:01:39 +0530

    Enhance BentoGridItem layout

--- components/ui/BentoGrid.tsx
+++ components/ui/BentoGrid.tsx
@@ -83,15 +83,25 @@ export const BentoGridItem = ({
           </BackgroundGradientAnimation>
         )}
 
+        <div className={cn(
+          titleClassName, 'group-hover/bento:translate-x-2 transition duration-200 relative md:h-full min-h-40 flex flex-col px-5 p-5 lg:p-10'
+        )}>
+            <div className="font-sans text-sm md:text-xs lg:text-base font-extralight text-[#c1c2d3] dark:text-neutral-300 z-10">
+              {description}
+            </div>
+
+            <div className="mt-2 mb-2 font-sans font-bold text-neutral-600 dark:text-neutral-200">
+              {title}
+            </div>
+
+
+        </div>
+
       </div>
 
       <div className="transition duration-200 group-hover/bento:translate-x-2">
-        <div className="mt-2 mb-2 font-sans font-bold text-neutral-600 dark:text-neutral-200">
-          {title}
-        </div>
-        <div className="font-sans text-xs font-normal text-neutral-600 dark:text-neutral-300">
-          {description}
-        </div>
+  
+
       </div>
     </div>
   );
