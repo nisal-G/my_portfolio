96c3049c2c710a0e5c26195a44ddc3d7b8a6ebb5
nisalG <nimgunathilaka021@gmail.com>
2025-11-23 19:34:02 +0530

    Update BentoGridItem title

--- components/ui/BentoGrid.tsx
+++ components/ui/BentoGrid.tsx
@@ -90,7 +90,7 @@ export const BentoGridItem = ({
               {description}
             </div>
 
-            <div className="mt-2 mb-2 font-sans font-bold text-neutral-600 dark:text-neutral-200">
+            <div className="mt-2 mb-2 font-sans font-bold text-lg lg:text-3xl max-w-96 z-10">
               {title}
             </div>
 
@@ -99,10 +99,6 @@ export const BentoGridItem = ({
 
       </div>
 
-      <div className="transition duration-200 group-hover/bento:translate-x-2">
-  
-
-      </div>
     </div>
   );
 };
