83640a0f194e70b60bfa31ca51a0db70ba2883d8
nisalG <nimgunathilaka021@gmail.com>
2025-12-03 17:16:02 +0530

    Add location link and improve layout in RecentProjects component ✅

--- components/RecentProjects.tsx
+++ components/RecentProjects.tsx
@@ -1,6 +1,7 @@
 import { projects } from '@/data';
 import React from 'react';
 import { PinContainer } from './ui/3d-pin';
+import { FaLocationArrow } from 'react-icons/fa';
 
 const RecentProjects = () => {
   return (
@@ -10,11 +11,11 @@ const RecentProjects = () => {
             <span className='text-purple-300'>recent projects</span>
         </h1>
 
-        <div className="flex flex-wrap items-center p-4 justify-center gap-16 mt-10">
+        <div className="flex flex-wrap items-center p-4 justify-center gap-x-24 gap-y-8 mt-10">
             {projects.map(({id, title, des, img, iconLists, link}) => (
-                <div key={id} className="lg:min-h-[32.5rem] h-[25rem] flex items-center justify-center sm:w-96 w-[80vw]">
-                    <PinContainer title={title} href={link}>
-                        <div className='relative flex items-center justify-center sm:w-96 w-[80vw] overflow-hidden h-[20vh] lg:h-[30vh] mb-10'>
+                <div key={id} className="sm:h-[41rem] h-[32rem] lg:min-h-[32.5rem] flex items-center justify-center sm:w-[570px] w-[80vw]">
+                    <PinContainer title={link} href={link}>
+                        <div className='relative flex items-center justify-center sm:w-[570px] w-[80vw] overflow-hidden sm:h-[40vh] h-[30vh] mb-10'>
                             <div className='relative w-full h-full overflow-hidden lg:rounded-3xl bg-[#13162d]'>
                                 <img src="/bg.png" alt="bg-img" />
                             </div>
@@ -31,10 +32,39 @@ const RecentProjects = () => {
                         <p className='lg:text-xl lg:font-normal font-light text-sm line-clamp-2'>
                             {des}
                         </p>
+
+                        <div className='flex items-center justify-between mt-7 mb-3'>
+                            <div className='flex items-center'>
+                                {iconLists.map((icon, index) => (
+                                    <div key={icon} className='border border-white/[0.2] rounded-full bg-black lg:w-10 lg:h-10 w-8 h-8 flex justify-center items-center' 
+                                    style={{
+                                        transform: `translateX(-${5 * index * 2}px)`
+                                    }}>
+                                        <img
+                                            src={icon}
+                                            alt={icon}
+                                            className='p-2'/>
+                                    </div>
+                                ))}
+                            </div>
+
+                            <div className='flex justify-center items-center'>
+                                <a 
+                                    href={link} 
+                                    target="_blank" 
+                                    rel="noopener noreferrer"
+                                    className='flex items-center lg:text-xl md:text-xs text-sm text-purple-300 hover:text-purple-400 transition-colors'
+                                >
+                                    Check Live Site
+                                    <FaLocationArrow className='ms-3' style={{color: "#CBACF9"}} />
+                                </a>
+                            </div>
+                        </div>
+
                     </PinContainer>
                 </div>
             ))}
-        </div>
+        </div> 
     </div>
   );
 }
