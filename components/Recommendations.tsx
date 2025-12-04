c0bc8ae3db9aeb12b3398afab1640bd2bb60c535
nisalG <nimgunathilaka021@gmail.com>
2025-12-04 15:00:00 +0530

    Add Recommendations and InfiniteMovingCards components for displaying testimonials

---
+++ components/Recommendations.tsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import { InfiniteMovingCards } from './ui/InfiniteMovingCards'
+import { testimonials } from '@/data'
+
+const Recommendations = () => {
+  return (
+    <div className='py-20' id='recommendations'>
+        <h1 className='heading'>
+            Words from Those I've <span className='text-purple-300'>Worked With</span>
+        </h1>
+
+        <div className="flex flex-col items-center">
+            <div className="h-[50vh] md:h-[30rem] rounded-md flex flex-col antialiased items-center relative overflow-hidden ">
+                <InfiniteMovingCards 
+                items={testimonials}
+                direction="right"
+                speed="slow"/>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Recommendations
+
