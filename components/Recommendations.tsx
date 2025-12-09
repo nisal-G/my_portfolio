1f3a1d93cebe0b1f905c78c28c1da304942d0d19
nisalG <nimgunathilaka021@gmail.com>
2025-12-09 10:26:42 +0530

    Enhance InfiniteMovingCards styling for better layout

--- components/Recommendations.tsx
+++ components/Recommendations.tsx
@@ -4,12 +4,12 @@ import { testimonials } from '@/data'
 
 const Recommendations = () => {
   return (
-    <div className='py-20' id='recommendations'>
+    <div className='py-20' id='testimonials'>
         <h1 className='heading'>
             Words from Those I've <span className='text-purple-300'>Worked With</span>
         </h1>
 
-        <div className="flex flex-col items-center">
+        <div className="flex flex-col items-center max-lg:mt-10">
             <div className="h-[50vh] md:h-[30rem] rounded-md flex flex-col antialiased items-center relative overflow-hidden ">
                 <InfiniteMovingCards 
                 items={testimonials}
